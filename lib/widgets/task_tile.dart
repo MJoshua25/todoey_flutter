45cad2acf5d3c328aa61b703551e4194fd661438
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2022-05-11 00:11:50 +0000

    le state du checkbox est bien géré mtn

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
+      title: Text('This is a task',
+          style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null,
+          )),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: checkboxCallback,
       ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox(
+      {Key? key,
+      required this.checkboxState,
+      required this.toggleCheckboxState})
+      : super(key: key);
+
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (bool? value) {
+        toggleCheckboxState(value);
+      },
     );
   }
 }
